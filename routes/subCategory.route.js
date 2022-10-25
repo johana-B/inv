f87ce4e9d780bdaa23f3cad45857aaf123d4f462
rouge9 <robelged1992@gmail.com>
2022-10-25 00:30:26 -0700

    subcatagory_changed

--- routes/subCategory.route.js
+++ routes/subCategory.route.js
@@ -1,91 +1,139 @@
-const express = require('express')
-const fs = require('fs')
+const express = require("express");
+const fs = require("fs");
 
-const subCategoryRouter = express.Router()
+const subCategoryRouter = express.Router();
 
-const SubCategory = require('../model/subCategory.model')
-const Category = require('../model/category.model')
-const Product = require('../model/products.model')
+const SubCategory = require("../model/subCategory.model");
+const Category = require("../model/category.model");
+const Product = require("../model/products.model");
 
-const asyncWrapper = require('../middleware/asyncWrapper')
+const asyncWrapper = require("../middleware/asyncWrapper");
 
-
-subCategoryRouter.get('/',asyncWrapper( async (req, res) =>{
-    const {name, sort,category} = req.query
-     filter = {}
-     if(category){
-        filter.category = category
-     }
-     if(name){
-        filter.name = {$regex:name, $options:'e'}
-     }
-
-    let result = SubCategory.find(filter)
-    
-    if(sort){
-        const sortList = sort.split(',').join(' ')
-        result = result.sort(sortList)
+subCategoryRouter.get(
+  "/",
+  asyncWrapper(async (req, res) => {
+    const { name, sort, category } = req.query;
+    filter = {};
+    if (category) {
+      filter.category = category;
+    }
+    if (name) {
+      filter.name = { $regex: name, $options: "e" };
     }
 
-    const subCategorys = await result.populate('category')
-    if (!subCategorys){
-        return res.status(404).json({msg:'there is no subcategory with this id'})
-    }
-    res.status(200).json({subCategorys})
-}))
+    let result = SubCategory.find(filter);
 
-subCategoryRouter.get('/:id',asyncWrapper( async (req, res) => {
-    const subCategory = await SubCategory.findById(req.params.id).populate('category')
+    if (sort) {
+      const sortList = sort.split(",").join(" ");
+      result = result.sort(sortList);
+    }
+
+    const subCategorys = await result.populate("category");
+    if (!subCategorys) {
+      return res
+        .status(404)
+        .json({ msg: "there is no subcategory with this id" });
+    }
+    res.status(200).json({ subCategorys });
+  })
+);
+
+subCategoryRouter.get(
+  "/:id",
+  asyncWrapper(async (req, res) => {
+    const subCategory = await SubCategory.findById(req.params.id).populate(
+      "category"
+    );
 
     if (!subCategory) {
-        res.status(404).json({ msg: 'The category with the given ID not exists'})
+      res
+        .status(404)
+        .json({ msg: "The category with the given ID not exists" });
     }
-    res.status(201).json({subCategory})
-}))
+    res.status(201).json({ subCategory });
+  })
+);
 
-subCategoryRouter.post('/',asyncWrapper( async (req, res) =>{
+subCategoryRouter.post(
+  "/",
+  asyncWrapper(async (req, res) => {
     const category = await Category.findById(req.body.category);
-    if (!category){
-        return res.status(400).send('Invalid Category')
+    if (!category) {
+      return res.status(400).send("Invalid Category");
     }
     let subCategory = new SubCategory({
-        name: req.body.name,
-        category:req.body.category
-    })
+      name: req.body.name,
+      category: req.body.category,
+    });
 
-    const subCategorys = await subCategory.save()
+    const subCategorys = await subCategory.save();
 
     if (!subCategorys)
-        return res.status(500).send('subCategory cannot be created')
+      return res.status(500).send("subCategory cannot be created");
 
-    res.status(200).json({subCategorys,msg:'subCategory is created successfully'})
-}))
+    res
+      .status(200)
+      .json({ subCategorys, msg: "subCategory is created successfully" });
+  })
+);
 
-subCategoryRouter.patch('/:id',asyncWrapper( async (req, res) =>{
-    const category = Category.find(req.query.category)
-    if(!category){
-        return res.status(404).json({msg:'invalid category'})
+subCategoryRouter.patch(
+  "/:id",
+  asyncWrapper(async (req, res) => {
+    const category = Category.find(req.query.category);
+    if (!category) {
+      return res.status(404).json({ msg: "invalid category" });
     }
     const subCategory = await SubCategory.findOneAndUpdate(req.query.id, {
-        name: req.body.name,
-        category: req.body.category,
-    }).populate('category')
-    if(!subCategory){
-        return res.status(404).json({msg:'invalid subCategory'}) 
+      name: req.body.name,
+      category: req.body.category,
+    }).populate("category");
+    if (!subCategory) {
+      return res.status(404).json({ msg: "invalid subCategory" });
     }
-    res.status(200).json({subCategory, msg:'subCategory is updated successfully'})
-}))
+    res
+      .status(200)
+      .json({ subCategory, msg: "subCategory is updated successfully" });
+  })
+);
 
-subCategoryRouter.delete('/:id',asyncWrapper( async (req, res) =>{
-    const {id:subCategoryID} = req.params
-    const subCategory = await SubCategory.findByIdAndDelete({_id:subCategoryID})
-    await Product.deleteMany({subCategory:subCategoryID})
-    if(!subCategory){
-        return res.status(404).json({msg:'invalid product'})  
+subCategoryRouter.delete(
+  "/:id",
+  asyncWrapper(async (req, res) => {
+    const { id: subCategoryID } = req.params;
+    const subCategory = await SubCategory.findByIdAndDelete({
+      _id: subCategoryID,
+    });
+    await Product.deleteMany({ subCategory: subCategoryID });
+    if (!subCategory) {
+      return res.status(404).json({ msg: "invalid product" });
     }
-    res.status(200).json({subCategory, msg:'subCategory is deleted successfully'}) 
-}))
+    res
+      .status(200)
+      .json({ subCategory, msg: "subCategory is deleted successfully" });
+  })
+);
 
+//delete products and images with subCategory id
+subCategoryRouter.delete(
+  "/delete/:id",
+  asyncWrapper(async (req, res) => {
+    const { id: subCategoryID } = req.params;
+    const subCategory = await SubCategory.findByIdAndDelete({
+      _id: subCategoryID,
+    });
+    const products = await Product.find({ subCategory: subCategoryID });
+    if (!subCategory) {
+      return res.status(404).json({ msg: "invalid product" });
+    }
+    products.forEach((product) => {
+      fs.unlinkSync(`./public/uploads/${product.image}`);
+    });
+    await Product.deleteMany({ subCategory: subCategoryID });
+    res
+      .status(200)
+      .json({ subCategory, msg: "subCategory is deleted successfully" });
+  })
+);
 
-
-module.exports = subCategoryRouter
+module.exports = subCategoryRouter;
